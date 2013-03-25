03451c5c1551e7fdda3d4835fff5d012d5cb930d
Daniel Puglisi <pulleasy@gmail.com>
2013-03-25 22:31:49 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DanielpuglisiCom::Application.config.secret_key_base = '9e54657ad830428c58898b7a3f997c0d24a89d640c91cd29e33f2b18a9522e0e364778829f4ba6295953d129d324dd5705cea53edbf80a3cb6ea641f7a9681b0'
