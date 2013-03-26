333851de7b098a090a6db00d2f36b264efc77146
Daniel Puglisi <pulleasy@gmail.com>
2013-03-26 08:54:54 +0100

    generated tag model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130325231308) do
+ActiveRecord::Schema.define(version: 20130326075402) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20130325231308) do
     t.string   "type"
     t.date     "published_at"
     t.boolean  "published"
+  end
+
+  create_table "tags", force: true do |t|
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "post_id"
   end
 
 end
