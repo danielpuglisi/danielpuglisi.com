16bf0654a7e3b79d0b173aef2caff486f49c21cb
Daniel Puglisi <pulleasy@gmail.com>
2013-03-26 08:57:24 +0100

    added associations and accessible attributes to models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ActiveRecord::Base
+  has_many :tags
+
+  attr_accessible :title, :content,
+    :published_at, :published,
+    :external_link, :type,
+    :slug
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,5 @@
 class Tag < ActiveRecord::Base
+  belongs_to :post
+
+  attr_accessible :post_id, :title
 end
