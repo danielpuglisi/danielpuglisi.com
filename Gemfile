ef082683f6db9c42379c5efdc546a0df28d987d1
Daniel Puglisi <pulleasy@gmail.com>
2013-03-27 10:15:03 +0100

    use psql in development

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ gem 'jquery-rails'
 gem 'friendly_id', github: "norman/friendly_id", branch: "rails4"
 gem 'redcarpet'
 gem 'kaminari'
+gem 'pg'
 
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20130326112220) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: true do |t|
     t.string   "title"
     t.text     "content"
