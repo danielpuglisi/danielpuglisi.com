05487b4b09454fd0214f25738f5c0d471ca2308c
Daniel Puglisi <pulleasy@gmail.com>
2013-03-28 17:28:53 +0100

    added different layouts to admin controller and application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  layout :layout
+
+  private
+  def layout
+    if is_a?(Devise::SessionsController)
+      "login"
+    else
+      "application"
+    end
+  end
 end
