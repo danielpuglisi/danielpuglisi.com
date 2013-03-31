e6a1528c6a1d2d127afa55c393e0804cd3193488
Daniel Puglisi <pulleasy@gmail.com>
2013-03-31 12:44:11 +0200

    removed .html from links in home page and added a redirect for /blog to /articles

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ DanielpuglisiCom::Application.routes.draw do
   get "/recommendations", to: "pages#recommendations"
 
   # Posts
+  get "/blog", to: redirect("/articles")
   get "/articles", to: "posts#index"
   get "/articles/:year/:month(/:day)/:id", to: "posts#show", as: :post,
     year:   /(19|20)\d{2}/,
