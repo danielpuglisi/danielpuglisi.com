bd876187b8e7671ec841642ebcae1f7ee6f71da5
Daniel Puglisi <pulleasy@gmail.com>
2013-06-21 12:56:06 +0200

    updating and creating a post should not redirect to posts index

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -20,7 +20,7 @@ class Admin::PostsController < AdminController
     @post = Post.new(post_params)
 
     if @post.save
-      redirect_to admin_posts_path, notice: 'Post was successfully created.'
+      redirect_to edit_admin_post_path(@post), notice: 'Post was successfully created.'
     else
       render action: 'new'
     end
@@ -28,7 +28,7 @@ class Admin::PostsController < AdminController
 
   def update
     if @post.update(post_params)
-      redirect_to admin_posts_path, notice: 'Post was successfully updated.'
+      redirect_to edit_admin_post_path(@post), notice: 'Post was successfully updated.'
     else
       render action: 'edit'
     end
