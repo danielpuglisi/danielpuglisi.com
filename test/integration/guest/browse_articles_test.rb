b623616142a54c51e1db702a5346c0ee998f2466
Daniel Puglisi <pulleasy@gmail.com>
2013-06-28 09:51:50 +0200

    test if article is returned

--- test/integration/guest/browse_articles_test.rb
+++ test/integration/guest/browse_articles_test.rb
@@ -8,4 +8,12 @@ class BrowseArticlesTest < ActionDispatch::IntegrationTest
     visit articles_path
     assert_equal ["A programming post", "A productivity post"], all(".entry header h2").map(&:text)
   end
+
+  test "should return article when header is clicked" do
+    post = create(:post, title: "Hepper jepper")
+    visit articles_path
+    click_link "Hepper jepper"
+    assert_equal "Hepper jepper", find(".entry header h2").text
+    assert_equal post_path(year: post.published_at.year, month: post.published_at.strftime("%m"), id: post), current_path
+  end
 end
