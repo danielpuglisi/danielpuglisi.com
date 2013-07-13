bff4ac799d2d3e27fa8aa2c96b7e255d3314738b
Daniel Puglisi <pulleasy@gmail.com>
2013-07-13 16:14:49 +0200

    generated links table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130628100648) do
+ActiveRecord::Schema.define(version: 20130713141413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,15 @@ ActiveRecord::Schema.define(version: 20130628100648) do
   add_index "impressions", ["impressionable_type", "impressionable_id", "session_hash"], name: "poly_session_index", using: :btree
   add_index "impressions", ["impressionable_type", "message", "impressionable_id"], name: "impressionable_type_message_index", using: :btree
   add_index "impressions", ["user_id"], name: "index_impressions_on_user_id", using: :btree
+
+  create_table "links", force: true do |t|
+    t.string   "name"
+    t.string   "url"
+    t.text     "description"
+    t.integer  "issue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "old_tags", force: true do |t|
     t.string   "title"
