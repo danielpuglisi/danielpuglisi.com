974fea914f67da6879f1fed2f942f3ced3dbce86
Daniel Puglisi <pulleasy@gmail.com>
2013-07-13 16:06:01 +0200

    removed legacy tags code

--- app/controllers/tags_controller.rb
+++
@@ -1,8 +0,0 @@
-class TagsController < ApplicationController
-
-  def show
-    @tag = Tag.find_by_slug(params[:id])
-    @posts = @tag.posts.published.page(params[:page]).per(10)
-    @title = @tag.title
-  end
-end
--- app/models/old_tag.rb
+++
@@ -1,5 +0,0 @@
-class OldTag < ActiveRecord::Base
-  self.table_name = "old_tags"
-  extend FriendlyId
-  friendly_id :title, use: :slugged
-end
