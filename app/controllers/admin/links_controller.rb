0b1826c6e9c3b259f7a1d372580f83b3c76baa18
Daniel Puglisi <pulleasy@gmail.com>
2013-07-14 14:26:24 +0200

    added link form and controller actions

--- app/controllers/admin/links_controller.rb
+++ app/controllers/admin/links_controller.rb
@@ -1,6 +1,46 @@
 class Admin::LinksController < AdminController
 
+  before_action :set_link, only: [:show, :edit, :update, :destroy]
+
   def index
     @links = Link.all
   end
+
+  def new
+    @link = Link.new
+  end
+
+  def edit
+  end
+
+  def create
+    @link = Link.new(link_params)
+
+    if @link.save
+      redirect_to admin_links_path, notice: "Link was successfully created."
+    else
+      render action: 'new'
+    end
+  end
+
+  def update
+    if @link.update(link_params)
+      redirect_to admin_links_path, notice: "Link was successfully updated."
+    else
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+    @link.destroy
+    redirect_to admin_links_path, notice: "Link was successfully destroyed."
+  end
+
+  private
+    def set_link
+      @link = Link.find(params[:id])
+    end
+    def link_params
+      params[:link].permit(:name, :url, :tag_list, :description, :issue)
+    end
 end
