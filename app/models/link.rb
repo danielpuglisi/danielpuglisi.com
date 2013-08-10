11f8bee1222baf745a58f0303c6ba320c46f53e0
Daniel Puglisi <pulleasy@gmail.com>
2013-08-10 19:18:34 +0200

    remove last forward slashes from links

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,9 @@
 class Link < ActiveRecord::Base
   acts_as_taggable rescue nil
+
+  validates :url, uniqueness: true
+
+  def url=(url)
+    write_attribute(:url, url.match(/(.*[^\/])\/*$/)[1])
+  end
 end
