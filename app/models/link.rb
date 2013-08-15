70db826707666dcefc68a19f989f1232bb7ddfd9
Daniel Puglisi <pulleasy@gmail.com>
2013-08-15 19:02:06 +0200

    link must have an url and a name

--- app/models/link.rb
+++ app/models/link.rb
@@ -4,6 +4,7 @@ class Link < ActiveRecord::Base
   include RankedModel
   ranks :issue_order, with_same: :issue_id
 
+  validates :name, :url, presence: true
   validates :url, uniqueness: true
 
   belongs_to :issue
