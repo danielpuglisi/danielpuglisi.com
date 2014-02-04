72f97f31bdfc409346500efcfc20f153031b2805
Daniel Puglisi <pulleasy@gmail.com>
2014-02-04 11:27:55 +0100

    add default text to issue when body text is empty on creation

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -5,4 +5,36 @@ class Issue < ActiveRecord::Base
 
   has_many :links
   accepts_nested_attributes_for :links, allow_destroy: true
+
+  after_create do
+    unless body.present?
+      update(body: <<EOF
+Welcome to Friday is Webday ##{number}.
+
+### Articles
+
+{% links #{number}, article %}
+
+### Demos and Code
+
+{% links #{number}, demo %}
+
+### Libraries and Tools
+
+{% links #{number}, library %}
+{% links #{number}, tool %}
+
+### Inspirations and Useful Stuff
+
+{% links #{number}, inspiration %}
+
+Found something useful? Give me a hand and share it with your friends and followers.
+
+If you have any cool project you would like to see in one of the next issues hit the reply button and show me what you got.
+
+Daniel
+EOF
+      )
+    end
+  end
 end
